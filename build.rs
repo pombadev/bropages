1947174551d270aefb6dde54450a830aae10ee78
pombadev <11157420+pombadev@users.noreply.github.com>
2022-12-31 01:32:02 -0300

    fix: getting version+hash

--- build.rs
+++ build.rs
@@ -1,17 +1,26 @@
-use std::{env, fs, path::Path, process::Command};
+use std::{
+    env, fs,
+    path::Path,
+    process::{Command, Stdio},
+};
 
 fn main() {
-    let output = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()
-        .unwrap();
+    let mut version = format!("v{}", env!("CARGO_PKG_VERSION"));
 
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    let version = format!("v{}+{}", env!("CARGO_PKG_VERSION"), git_hash);
+    if let Ok(out) = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .stdout(Stdio::piped())
+        .output()
+    {
+        if let Ok(hash) = String::from_utf8(out.stdout) {
+            version.push('+');
+            version.push_str(hash.trim());
+        }
+    }
 
     fs::write(
-        Path::new(&env::var("OUT_DIR").unwrap()).join("version"),
+        Path::new(&env::var("OUT_DIR").expect("OUT_DIR to be defined")).join("version"),
         version.trim(),
     )
-    .unwrap();
+    .unwrap_or_else(|err| panic!("create version file with: {version} failed because: {err}"));
 }
